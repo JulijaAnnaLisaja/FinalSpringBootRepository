5ac05cd1c79742915b8c7431530e61b5911f7336
JulijaAnnaLisaja <julija.anna.lisaja@accenture.com>
2021-01-31 20:50:15 +0200

    Controller Tests

--- src/main/java/com/cse/spring/entity/BaseEntity.java
+++ src/main/java/com/cse/spring/entity/BaseEntity.java
@@ -22,5 +22,4 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private final LocalDate creationDate = LocalDate.now();
 }
